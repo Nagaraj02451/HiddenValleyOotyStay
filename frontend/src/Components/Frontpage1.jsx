64b84aaa7a83b7f56c5cd3edcec2a0482c5664f4
Nagaraj <124070128+Nagaraj02451@users.noreply.github.com>
2025-03-14 22:30:25 +0530

    Update Frontpage1.jsx

--- frontend/src/Components/Frontpage1.jsx
+++ frontend/src/Components/Frontpage1.jsx
@@ -5,21 +5,35 @@ import Footer1 from './Footer1'
 import Footer2 from './Footer2'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Link, useNavigate } from 'react-router-dom'
 
-const Frontpage1 = () => {
-   const [date, setDate] = useState(new Date());
+const Frontpage1 = ({ addBase, adult , pizza , child , dateone , datetwo }) => {
+  const bases = ['SUNSET COTTAGE', 'SUNSET VALLEY VILLA', 'RIVERSIDE' , 'NETHRA' , 'TINYHOUSE'];
+  const [delivery, setdelivery] = useState("");
+//   console.log(delivery , "uiyigf");
+const navigate = useNavigate();
+  
+   const [datefirst, setDate] = useState(new Date());
    const [date1, setDate1] = useState(new Date());
    const [count, setcount] = useState(0);
    const [count1, setcount1] = useState(0);
+//   console.log(date1.toString().slice(0,15), date , "uiyigf");
+
+
    const increment =()=>{
       setcount(count+1)
+      adult(count+1)
       if(count >= 3){
         alert("Only 5 Members allowed")
       }
       }
-      
+      const hgcytfuyf =()=>{
+         dateone(datefirst.toString().slice(0,15))
+
+      }
       const decrement =()=>{
       setcount(count-1)
+      adult(count-1)
       if(count === 0){
          setcount(0)
          alert("Select alteast one")
@@ -27,6 +41,7 @@ const Frontpage1 = () => {
       }
    const increment1 =()=>{
       setcount1(count1+1)
+      child(count1+1)
       if(count1 >= 3){
         alert("Only 5 Members allowed")
       }
@@ -34,11 +49,29 @@ const Frontpage1 = () => {
       
       const decrement1 =()=>{
       setcount1(count1-1)
+      child(count1+1)
       if(count1 === 0){
          setcount1(0)
          alert("Select alteast one")
        }
       }
+     const hgfdutydc =()=>{
+   
+      datetwo(date1.toString().slice(0,15))
+
+      if(delivery === "SUNSET COTTAGE"){
+         navigate("/sunsetcottageBooking")
+      }else if(delivery === "SUNSET VALLEY VILLA"){
+         navigate("/sunsetVillaBooking")
+      }else if(delivery === "RIVERSIDE"){
+         navigate("/riversideBooking")
+      }else if(delivery === "NETHRA"){
+         navigate("/nethraBooking")
+      }else if(delivery === "TINYHOUSE"){
+         navigate("/tinyhouseBooking")
+      }
+
+     }
    return (
       <>
          <div className='fr1'>
@@ -58,26 +91,34 @@ const Frontpage1 = () => {
 
                      <div className='ufygu'>
                         <p className='iuyrtf87iyug'>Book your stay with us</p>
-                        <select>
-                        <option>SUNSET COTTAGE</option>
-      <option>SUNSET VALLEY VILLA</option>
-      <option>RIVERSIDE</option>
-      <option>NETHRA </option>
-      <option>TINYHOUSE </option>
-                        </select>
+                        <select  value={delivery} onChange={(e)=>setdelivery(e.target.value)}>
+       
+
+
+       {bases.map(base => {
+         let spanClass = pizza.base === base ? 'active' : '';
+         return (
+           <option key={base} onClick={() => addBase(base)}>
+             <span className={spanClass}>{ base }</span>
+           </option>
+         )
+       })}
+
+
+       </select>
                         <div className='uytfdu6ty'>
                         <div className='yutdfrt6uy'>
          {/* <div>ugyhf</div> */}
       <span className='uytrfd6u7tyf'></span>
       {/* <input type='text' placeholder='CHECK IN ' /> */}
-      <DatePicker id='uytr7iy' placeholderText='CHECK IN' selected={date} onChange={(date) => setDate(date)} />
+      <DatePicker id='uytr7iy' placeholderText='CHECK IN' selected={datefirst} onChange={(datefirst) => setDate(datefirst)} />
       {/* <input type='text' placeholder='CHECK OUT' /> */}
    </div>
-                        <div className='yutdfrt6uy'>
+                        <div className='yutdfrt6uy' onClick={hgcytfuyf}>
          {/* <div>ugyhf</div> */}
       <span className='uytrfd6u7tyf'></span>
       {/* <input type='text' placeholder='CHECK OUT ' /> */}
-      <DatePicker id='uytr7iy' placeholderText='CHECK OUT' selected={date1} onChange={(date) => setDate1(date)} />
+      <DatePicker id='uytr7iy'  placeholderText='CHECK OUT' selected={date1} onChange={(date) => setDate1(date)} />
       {/* <input type='text' placeholder='CHECK OUT' /> */}
    </div>
                            {/* <input type='text' placeholder='CHECK IN ' />
@@ -105,8 +146,8 @@ const Frontpage1 = () => {
 
                         </div>
 
-                      <a href='site' className='ytrfg7yu'>  <div className='uytedr756fg'><span className='uytrfd6u7tyf1'></span>SEARCH</div>
-                      </a>
+                      <div  className='ytrfg7yu' onClick={hgfdutydc}  >  <div className='uytedr756fg'     ><span className='uytrfd6u7tyf1'></span><div   className='iuyfgt6ty'>SEARCH</div></div>
+                      </div>
                      </div>
 
 
@@ -157,7 +198,7 @@ const Frontpage1 = () => {
    <div className='yutdfrt6uy'>
          {/* <div>ugyhf</div> */}
       <span className='uytrfd6u7tyf'></span>
-      <DatePicker id='uytr7iy' placeholderText='CHECK IN' selected={date} onChange={(date) => setDate(date)} />
+      <DatePicker id='uytr7iy' placeholderText='CHECK IN' selected={datefirst} onChange={(datefirst) => setDate(datefirst)} />
 
       {/* <input type='text' placeholder='CHECK OUT' /> */}
    </div>
