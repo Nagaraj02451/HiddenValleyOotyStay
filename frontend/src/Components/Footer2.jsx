359aa2c4a3b92ac390e57b7297419e9ee0f7cdfe
Nagaraj <124070128+Nagaraj02451@users.noreply.github.com>
2025-03-15 08:35:07 +0530

    Update Footer2.jsx

--- frontend/src/Components/Footer2.jsx
+++ frontend/src/Components/Footer2.jsx
@@ -8,10 +8,10 @@ const Footer2 = () => {
        
        <div className='foot12'>
           
-       <a href='/privacyPolicy'><div className='foot2a'>Privacy Policy</div></a>
-       <a href='/termsAndConditions'><div className='foot2a'>Terms and Conditions</div></a>
-       <a href='/refundPolicy'><div className='foot2a'>Refund Policy</div></a>
-       <a href=''><div className='foot2a'>Sitemap</div></a>
+       <a href='/privacyPolicy'><div className='foot2ab'>Privacy Policy</div></a>
+       <a href='/termsAndConditions'><div className='foot2ab'>Terms and Conditions</div></a>
+       <a href='/refundPolicy'><div className='foot2ab'>Refund Policy</div></a>
+       <a href=''><div className='foot2ab'>Sitemap</div></a>
 
        </div>
        
