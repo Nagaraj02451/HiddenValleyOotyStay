b7e7a90570f1efe24dbc290c42ef4d98cad859ae
Nagaraj <124070128+Nagaraj02451@users.noreply.github.com>
2025-04-12 11:00:06 +0530

    Add files via upload

---
+++ frontend/src/Components/Success/Success.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Success.css"
+import Navbar1 from '../Navbar1'
+import Footer1 from '../Footer1'
+import Footer2 from '../Footer2'
+
+function Success() {
+  return (
+    <>
+        {/* <div className='nav1'> */}
+
+    {/* <Navbar1 /> */}
+    {/* </div> */}
+    <div className='container d-flex justify-content-center success align-items-center'>
+    <div>
+    <div className='container d-flex justify-content-center hjgvjhvhv'>
+<img src='/Img/sui.png' alt='' />
+    </div>
+  <div className='paoo mt-5'>Payment Was Successful</div>
+  <div className='oplop d-flex justify-content-center  mt-4'>Your Booking is Confirm......</div>
+ <a href="/home"> <div className='koppo d-flex justify-content-center mt-4'>Redirecting to Home Page</div></a>
+</div>
+</div>
+    <Footer2 />
+    </>
+  )
+}
+
+export default Success
