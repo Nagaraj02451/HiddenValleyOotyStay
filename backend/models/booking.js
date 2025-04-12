0a3d17e806e0e210550d08bfa8a4539dfe472c07
Nagaraj <124070128+Nagaraj02451@users.noreply.github.com>
2025-04-12 11:03:01 +0530

    Update booking.js

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -1,82 +1,67 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
-const booking = new mongoose.Schema({
-    name: {
-        type: String,
-        // required: [true, "Please enter product name"],
-        trim: true,
-        maxLength: [20, "name cannot exceed 20 characters"]
-    },
-    email: {
-        type: String,
-        // required: [true, "Please enter product name"],
-        trim: true,
-        maxLength: [20, "name cannot exceed 20 characters"]
-    },
-    phone: {
-        type: String,
-        // required: [true, "Please enter product name"],
-        trim: true,
-        maxLength: [20, "name cannot exceed 20 characters"]
-    },
-    room:{
-        type: String,
-        required: [true, 'Please enter email'],
-        unique: true,
-        validate: [20, 'Please enter valid email address']
-    },
-    tax: {
-        type: String,
-        // required: [true, "Please enter product name"],
-        trim: true,
-        // maxLength: [100, "Message cannot exceed 100 characters"]
-    },
-    total: {
-        type: String,
-        // required: [true, "Please enter product name"],
-        trim: true,
-        // maxLength: [100, "Message cannot exceed 100 characters"]
-    },
-    date: {
-        type: String,
-        // required: [true, "Please enter product name"],
-        trim: true,
-        // maxLength: [100, "Message cannot exceed 100 characters"]
-    },
-    date2: {
-        type: String,
-        // required: [true, "Please enter product name"],
-        trim: true,
-        // maxLength: [100, "Message cannot exceed 100 characters"]
-    },
-    adult: {
-        type: String,
-        // required: [true, "Please enter product name"],
-        trim: true,
-        // maxLength: [100, "Message cannot exceed 100 characters"]
-    },
-    child: {
-        type: String,
-        // required: [true, "Please enter product name"],
-        trim: true,
-        // maxLength: [100, "Message cannot exceed 100 characters"]
-    },
-    stay: {
-        type: String,
-        // required: [true, "Please enter product name"],
-        trim: true,
-        // maxLength: [100, "Message cannot exceed 100 characters"]
-    },
-    selectedroom: {
-        type: String,
-        // required: [true, "Please enter product name"],
-        trim: true,
-        // maxLength: [100, "Message cannot exceed 100 characters"]
-    },
-})
+const bookingSchema = new mongoose.Schema({
+  isPaid: {
+    type: Boolean,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  razorpay: {
+    orderId: String,
+    paymentId: String,
+    signature: String
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  room: {
+    type: String,
+    required: true
+  },
+  tax: {
+    type: Number,
+    required: true
+  },
+  total: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  date2: {
+    type: Date,
+    required: true
+  },
+  adult: {
+    type: Number,
+    required: true
+  },
+  child: {
+    type: Number,
+    required: true
+  },
+  stay: {
+    type: Number,
+    required: true
+  },
+  selectedroom: {
+    type: String,
+    required: true
+  }
+});
 
-let model =  mongoose.model('news', booking);
-
-
-module.exports = model;
+module.exports = mongoose.model('Booking', bookingSchema);
