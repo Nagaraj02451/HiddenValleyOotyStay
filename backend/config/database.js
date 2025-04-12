69c23ce8042ec29abf4414b2ae2dbdc282277d54
Nagaraj <124070128+Nagaraj02451@users.noreply.github.com>
2025-04-12 22:19:13 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const connectDatabase = () => {
     mongoose.connect(process.env.DB_LOCAL_URI, {
-        useUnifiedTopology: true,
         useNewUrlParser: true
     }).then(con => {
         console.log(`MongoDB is connected to the host: ${con.connection.host} `)
