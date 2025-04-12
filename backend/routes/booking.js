a4edf0afcabe3aeafeb7c13c37effd927f73feb4
Nagaraj <124070128+Nagaraj02451@users.noreply.github.com>
2025-04-12 11:02:35 +0530

    Update booking.js

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const { booking } = require('../controllers/bookingcontrol');
+const { keyrazorpay } = require('../controllers/bookingcontrol');
+const { createbooking } = require('../controllers/bookingcontrol');
 // const { default: Booking } = require('../../frontend/src/Components/Booking');
 
 
 const router = express.Router();
 
-router.route('/booking').post(booking);
+// router.route('/booking').post(booking);
+router.route('/get-razorpay-key').get(keyrazorpay);
+router.route('/create-order').post(createbooking);
+router.route('/pay-order').post(booking);
 
 
 module.exports = router;
