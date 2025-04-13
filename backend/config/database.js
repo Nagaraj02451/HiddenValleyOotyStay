77b2b275ad9e7f1d4616e79599baf68aa863d05c
Nagaraj <124070128+Nagaraj02451@users.noreply.github.com>
2025-04-13 11:33:36 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
-const connectDatabase = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI).then(con => {
-        console.log(`MongoDB is connected to the host: ${con.connection.host} `)
-    })
+const connectDatabase = async() => {
+    try{
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.DB_LOCAL_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log(`MongoDB is connected  `)
+    }
+     catch(err){
+        console.error(err.message);
+        // make the process fail
+        process.exit(1);
+     }
 }
 
 module.exports = connectDatabase;
